2e33fba885e0475d938d22bf34c6dcb078984081
myoungsubsim <rop2001@gmail.com>
2020-04-02 16:57:35 +0900

    변수명 변경

--- src/ch01/example/index.js
+++ src/ch01/example/index.js
@@ -1,4 +1,14 @@
 export function statement(invoice, plays) {
+    let result = `청구 내역 (고객명: ${invoice.customer})\n`;
+
+    for (let perf of invoice.performances) {
+        result += `${playFor(perf).name}: ${usd(amountFor(perf)/100)} (${perf.audience}석) \n`;
+    }
+    
+    result += `총액: ${usd(totalAmount()/100)}\n`;
+    result += `적립 포인트: ${totalVolumeCredits()}점 \n`;
+    return result;
+
     function amountFor(aPerformance) {
         let result = 0;
         switch (playFor(aPerformance).type) {
@@ -27,13 +37,31 @@ export function statement(invoice, plays) {
     }
 
     function volumeCreditsFor(perf) {
-        let volumeCredits = 0;
-        volumeCredits += Math.max(perf.audience - 30, 0);
+        let result = 0;
+        result += Math.max(perf.audience - 30, 0);
         if ('comedy' === playFor(perf).type) {
-            volumeCredits += Math.floor(perf.audience / 5);
+            result += Math.floor(perf.audience / 5);
         }
 
-        return volumeCredits;
+        return result;
+    }
+
+    function totalVolumeCredits() {
+        let result = 0;
+
+        for (let perf of invoice.performances) {
+            result += volumeCreditsFor(perf);
+        }
+
+        return result;
+    }
+
+    function totalAmount() {
+        let result = 0;
+        for (let perf of invoice.performances) {
+            result += amountFor(perf);
+        }
+        return result;
     }
 
     function usd(aNumber) {
@@ -41,22 +69,5 @@ export function statement(invoice, plays) {
             style: 'currency', currency: 'USD', minimumFractionDigits: 2
         }).format(aNumber);
     }
-
-    let totalAmount = 0;
-    let volumeCredits = 0;
-    let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-
-    for (let perf of invoice.performances) {
-        result += `${playFor(perf).name}: ${usd(amountFor(perf)/100)} (${perf.audience}석) \n`;
-        totalAmount += amountFor(perf);
-    }
-    
-    for (let perf of invoice.performances) {
-        volumeCredits += volumeCreditsFor(perf);
-    }
-
-    result += `총액: ${usd(totalAmount/100)}\n`;
-    result += `적립 포인트: ${volumeCredits}점 \n`;
-    return result;
 }
 
