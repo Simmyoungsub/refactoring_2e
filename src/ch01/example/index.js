3d7da4faaffc25746bd3653247886a37a2073c3f
myoungsubsim <rop2001@gmail.com>
2020-04-02 17:05:11 +0900

    매개변수(invoice)삭제

--- src/ch01/example/index.js
+++ src/ch01/example/index.js
@@ -1,12 +1,14 @@
 export function statement(invoice, plays) {
     const statementData = {};
-    return renderPlainText(statementData, invoice, plays);
+    statementData.customer = invoice.customer;
+    statementData.performances = invoice.performances;
+    return renderPlainText(statementData, plays);
 }
 
-function renderPlainText(data, invoice, plays) {
-    let result = `청구 내역 (고객명: ${invoice.customer})\n`;
+function renderPlainText(data, plays) {
+    let result = `청구 내역 (고객명: ${data.customer})\n`;
 
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
         result += `${playFor(perf).name}: ${usd(amountFor(perf)/100)} (${perf.audience}석) \n`;
     }
     
@@ -54,7 +56,7 @@ function renderPlainText(data, invoice, plays) {
     function totalVolumeCredits() {
         let result = 0;
 
-        for (let perf of invoice.performances) {
+        for (let perf of data.performances) {
             result += volumeCreditsFor(perf);
         }
 
@@ -63,7 +65,7 @@ function renderPlainText(data, invoice, plays) {
 
     function totalAmount() {
         let result = 0;
-        for (let perf of invoice.performances) {
+        for (let perf of data.performances) {
             result += amountFor(perf);
         }
         return result;
