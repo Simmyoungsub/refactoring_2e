a627616c0981bc1ecf5a1f8b0786799f747b909c
myoungsubsim <rop2001@gmail.com>
2020-04-02 17:22:18 +0900

    totalVolumeCredits 메서드 이동

--- src/ch01/example/index.js
+++ src/ch01/example/index.js
@@ -3,6 +3,7 @@ export function statement(invoice, plays) {
     statementData.customer = invoice.customer;
     statementData.performances = invoice.performances.map(enrichPerformance);
     statementData.totalAmount = totalAmount(statementData);
+    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
     return renderPlainText(statementData);
 
     function enrichPerformance(aPerformane) {
@@ -46,20 +47,16 @@ export function statement(invoice, plays) {
         }
         return result;
     }
-}
 
-function renderPlainText(data) {
-    let result = `청구 내역 (고객명: ${data.customer})\n`;
+    function totalVolumeCredits(data) {
+        let result = 0;
 
-    for (let perf of data.performances) {
-        result += `${perf.play.name}: ${usd(perf.amount/100)} (${perf.audience}석) \n`;
+        for (let perf of data.performances) {
+            result += volumeCreditsFor(perf);
+        }
+
+        return result;
     }
-    
-    result += `총액: ${usd(data.totalAmount/100)}\n`;
-    result += `적립 포인트: ${totalVolumeCredits()}점 \n`;
-    return result;
-
-    
 
     function volumeCreditsFor(perf) {
         let result = 0;
@@ -70,18 +67,18 @@ function renderPlainText(data) {
 
         return result;
     }
+}
 
-    function totalVolumeCredits() {
-        let result = 0;
+function renderPlainText(data) {
+    let result = `청구 내역 (고객명: ${data.customer})\n`;
 
-        for (let perf of data.performances) {
-            result += volumeCreditsFor(perf);
-        }
-
-        return result;
+    for (let perf of data.performances) {
+        result += `${perf.play.name}: ${usd(perf.amount/100)} (${perf.audience}석) \n`;
     }
-
     
+    result += `총액: ${usd(data.totalAmount/100)}\n`;
+    result += `적립 포인트: ${data.totalVolumeCredits}점 \n`;
+    return result;
 
     function usd(aNumber) {
         return new Intl.NumberFormat('en-US', {
