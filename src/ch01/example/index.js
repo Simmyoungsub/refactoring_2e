6290d65fdfd7e30ac6e7d3607c5cf54ded80dfae
myoungsubsim <rop2001@gmail.com>
2020-04-02 14:02:02 +0900

    volumeCreditsFor 함수 추출

--- src/ch01/example/index.js
+++ src/ch01/example/index.js
@@ -33,10 +33,18 @@ export function statement(invoice, plays) {
         return plays[aPerformance.playID];
     }
 
-    for (let perf of invoice.performances) {
+    function volumeCreditsFor(perf) {
+        let volumeCredits = 0;
         volumeCredits += Math.max(perf.audience - 30, 0);
+        if ('comedy' === playFor(perf).type) {
+            volumeCredits += Math.floor(perf.audience / 5);
+        }
 
-        if ('comedy' === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+        return volumeCredits;
+    }
+
+    for (let perf of invoice.performances) {
+        volumeCredits += volumeCreditsFor(perf);
 
         result += `${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience}석) \n`;
         totalAmount += amountFor(perf);
