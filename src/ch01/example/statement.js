46a7a23ba27fa06d4e704885d41b1527bb69d224
myoungsubsim <rop2001@gmail.com>
2020-04-02 21:04:19 +0900

    html태그 적용

--- src/ch01/example/statement.js
+++ src/ch01/example/statement.js
@@ -5,19 +5,21 @@ export function htmlStatement(invoice, plays) {
 }
 
 function renderHtml(data) {
-    let result = `청구 내역 (고객명: ${data.customer})\n`;
-
+    let result = `<h1>청구 내역 (고객명: ${data.customer})</h1>\n`;
+    result += `<table>\n`;
+    result += `<tr><th>연극</th><th>좌석수</th><th>금액</th></tr>`
     for (let perf of data.performances) {
-        result += `${perf.play.name}: ${usd(perf.amount / 100)} (${perf.audience}석) \n`;
+        result += `<tr><td>${perf.play.name}</td><td>${usd(perf.amount / 100)}</td><td>(${perf.audience}석)</td>\n`;
     }
+    result += `</table>`;
 
-    result += `총액: ${usd(data.totalAmount / 100)}\n`;
-    result += `적립 포인트: ${data.totalVolumeCredits}점 \n`;
+    result += `<p>총액: <em>${usd(data.totalAmount / 100)}</em></p>\n`;
+    result += `<p>적립 포인트: <em>${data.totalVolumeCredits}점</em></p> \n`;
     return result;
+}
 
-    function usd(aNumber) {
-        return new Intl.NumberFormat('en-US', {
-            style: 'currency', currency: 'USD', minimumFractionDigits: 2
-        }).format(aNumber);
-    }
+function usd(aNumber) {
+    return new Intl.NumberFormat('en-US', {
+        style: 'currency', currency: 'USD', minimumFractionDigits: 2
+    }).format(aNumber);
 }
