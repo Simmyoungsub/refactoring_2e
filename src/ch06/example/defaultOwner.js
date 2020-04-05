a7681c1bd2064520e187a948651553d25b723645
myoungsubsim <rop2001@gmail.com>
2020-04-05 21:16:37 +0900

    레코드 캡슐화 하기

--- src/ch06/example/defaultOwner.js
+++ src/ch06/example/defaultOwner.js
@@ -1,9 +1,21 @@
 let defaultOwner = {firstName: 'sim', lastName: 'myoungsub'};
 
 export function getDefaultOwner() {
-    return {...defaultOwner};
+    return new Person(defaultOwner);
 };
 
 export function setDefaultOwner(arg) {
     defaultOwner = arg;
 };
+
+class Person {
+    constructor(data) {
+        this._firstName = data.firstName;
+        this._lastName = data.lastName;
+    }
+
+    get firstName() {return this._firstName;}
+    set firstName(name) {return this._firstName = name;} 
+    get lastName() {return this._lastName;}
+    set lastName(name) {return this._lastName = name;}
+}
