09f4048f485e848b59ac105d716392ed47692abe
myoungsubsim <rop2001@gmail.com>
2020-04-05 21:42:59 +0900

    매개변수 객체 만들기 리팩토링

--- src/ch06/example/reading.test.js
+++ src/ch06/example/reading.test.js
@@ -1,4 +1,4 @@
-import {readingsOutsideRange} from './reading';
+import {readingsOutsideRange, NumberRange} from './reading';
 import {expect} from 'chai';
 
 describe('readings', function() {
@@ -19,13 +19,14 @@ describe('readings', function() {
 
         operatingPlan = {
             temperatureFloor: 50, // 최저 온도
-            temperatureCeil: 55, // 최고 온도
+            temperatureCeil: 52, // 최고 온도
         };
 
     });
 
     it ('readingsOutsideRange', function() {
-        const alerts = readingsOutsideRange(station, operatingPlan.temperatureFloor, operatingPlan.temperatureCeil);
-        expect(alerts.length).eq(2);
+        const range = new NumberRange(operatingPlan.temperatureFloor, operatingPlan.temperatureCeil);
+        const alerts = readingsOutsideRange(station, range);
+        expect(alerts.length).eq(4);
     });
 });
