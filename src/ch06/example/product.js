7a2af52c147f29c342d769a36a62d99829ac9a51
myoungsubsim <rop2001@gmail.com>
2020-04-06 23:23:29 +0900

    단계 쪼개기 리팩토링

--- src/ch06/example/product.js
+++ src/ch06/example/product.js
@@ -1,9 +1,23 @@
-export function priceOrder(product, quntity, shippingMethod) {
-    const basePrice = product.basePrice * quntity;
-    const discount = Math.max(quntity - product.discountThreshold, 0) * product.basePrice * product.discountRate;
-    const shippingPerCase = (basePrice > shippingMethod.discountThreshold) ?
+export function priceOrder(product, quantity, shippingMethod) {
+    const priceData = calculatePricingData(product, quantity); // 중간 데이터 구조
+    return applyShipping(priceData, shippingMethod);
+}
+
+function calculatePricingData(product, quantity) {
+    const basePrice = product.basePrice * quantity;
+    const discount = Math.max(quantity - product.discountThreshold, 0) * product.basePrice * product.discountRate;
+    
+    return {
+        basePrice,
+        quantity,
+        discount,
+    };
+}
+
+function applyShipping(priceData, shippingMethod) {
+    const shippingPerCase = (priceData.basePrice > shippingMethod.discountThreshold) ?
         shippingMethod.discountedFee : shippingMethod.feePerCase;
-    const shippingCost = quntity * shippingPerCase;
-    const price = basePrice - discount + shippingCost;
+    const shippingCost = priceData.quantity * shippingPerCase;
+    const price = priceData.basePrice - priceData.discount + shippingCost;
     return price;
 }
